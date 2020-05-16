a393bab931f0ed7b7557c5958c5ab64e3bcdce99
dragonfly161518 <por.nattapat.01@gmail.com>
2020-05-16 15:50:32 +0700

    add cart services

---
+++ src/services/cart/index.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+
+export const CartContext = React.createContext({});
+export const useCart = () => useContext(CartContext);
+export const CartProvider = ({ children }) => {
+  const [items, setItems] = useState([]);
+
+  const addCartItems = (addedItems) => {
+    const newItems = [...items, ...addedItems];
+    setItems(newItems);
+  };
+
+  const removeCartItem = () => {};
+
+  return (
+    <CartContext.Provider value={{ items, addCartItems }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
