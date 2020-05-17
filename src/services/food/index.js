ab094fb35932a29dd2692206df57111b6ca8995e
dragonfly161518 <por.nattapat.01@gmail.com>
2020-05-17 19:18:50 +0700

    change dir

--- src/services/food/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-
-export const FoodContext = React.createContext({});
-export const useFood = () => useContext(FoodContext);
-export const FoodProvider = ({ children }) => {
-  const [restaurants, setRestaurants] = useState();
-
-  return <FoodContext.Provider value={{}}>{children}</FoodContext.Provider>;
-};
