bd8b032b992edbdc90d230f0c4057efef4b3deff
PoomSC <poomsc@users.noreply.github.com>
2020-05-29 00:08:37 +0700

    nav bar

--- src/Containers/AppHome/index.js
+++ src/Containers/AppHome/index.js
@@ -7,23 +7,6 @@ import useSheet from "./../../utils/useSheet"
 const AppHomeContainer = () => {
   const { data, error, loading } = useSheet("Index")
 
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState();
-  // const [data, setData] = useState();
-
-  // useEffect(() => {
-  //   const initFunc = async () => {
-  //     try {
-  //       const data = await fetchData();
-  //       setLoading(false);
-  //       setData(data);
-  //     } catch (e) {
-  //       setError(e);
-  //     }
-  //   };
-  //   initFunc();
-  // }, []);
-
   if (error) {
     console.log(error)
     return <div>error</div>;
