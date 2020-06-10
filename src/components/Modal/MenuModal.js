4b7618b31b4256b764dc231f32dfeefd676f3bd6
PoomSC <poomsc@users.noreply.github.com>
2020-06-10 20:30:46 +0700

    styled

--- src/components/Modal/MenuModal.js
+++ src/components/Modal/MenuModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Modal from './Modal'
 
 import { MdAdd, MdClose } from 'react-icons/md'
+import { FiShoppingCart } from 'react-icons/fi'
 
 const ButtonIcon = ({ children, handleClick }) => {
   return (
@@ -25,8 +26,8 @@ const MenuModal = ({ open, handleClose, data = {}, handleAddItem }) => {
 
   return (
     <Modal open={open} handleClose={handleClose}>
-      <div className="w-1/3 p-5 bg-white z-50 m-auto">
-        <h1 className="tracking-widest text-xl mb-1">{menuName}</h1>
+      <div className="w-full lg:w-1/3 p-5 bg-white z-50 m-auto">
+        <h1 className="tracking-widest text-xl mb-1 font-medium">{menuName}</h1>
         <div className="flex items-center justify-between mb-2">
           <p className="text-lg">จำนวน</p>
           <input
@@ -38,12 +39,12 @@ const MenuModal = ({ open, handleClose, data = {}, handleAddItem }) => {
             value={amount}
           />
         </div>
-        <div className="flex items-center justify-between">
-          <ButtonIcon handleClick={handleClose}>
+        <div className="flex justify-center">
+          {/* <ButtonIcon handleClick={handleClose}>
             <MdClose />
-          </ButtonIcon>
+          </ButtonIcon> */}
           <ButtonIcon handleClick={handleSubmit}>
-            <MdAdd />
+            <FiShoppingCart className="mx-3"/>
           </ButtonIcon>
         </div>
       </div>
