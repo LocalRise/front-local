8487b29ef59bfbf2743f3a047fdc60503f31397c
PoomSC <poomsc@users.noreply.github.com>
2020-06-10 17:03:08 +0700

    edit style

--- src/components/MerchantList/MenuList/MenuItem.js
+++ src/components/MerchantList/MenuList/MenuItem.js
@@ -5,48 +5,47 @@ const MenuItem = ({
   handleSelect,
 }) => {
   return (
+    // <div
+    //   className="shadow-lg cursor-pointer hover:shadow-2xl"
+    //   onClick={() => handleSelect(id)}
+    // >
+    //   {menuImage && (
+    //     <div className="h-40">
+    //       <img
+    //         alt={menuName}
+    //         className="object-cover object-center w-full h-full block"
+    //         src={menuImage}
+    //       />
+    //     </div>
+    //   )}
+    //   <div className="mt-2 p-2">
+    //     {/* <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+    //       CATEGORY
+    //     </h3> */}
+    //     <h2 className="text-gray-900 title-font text-lg font-medium">
+    //       {menuName}
+    //     </h2>
+    //     <p className="mt-1">{menuPrice} THB</p>
+    //   </div>
+    // </div>
     <div
-      className="shadow-lg cursor-pointer hover:shadow-2xl"
+      class={` w-full flex border-b border-grey-light ${
+        menuImage === '' ? 'h-16' : ''
+        } md:my-2 mx-auto md:rounded-r md:shadow-md md:border-0`}
       onClick={() => handleSelect(id)}
     >
       {menuImage && (
-        <div className="h-40">
-          <img
-            alt={menuName}
-            className="object-cover object-center w-full h-full block"
-            src={menuImage}
-          />
-        </div>
+        <div
+          class="m-1 h-20 w-1/4 flex-none bg-cover rounded text-center overflow-hidden"
+          style={{ backgroundImage: `url('${menuImage}')` }}
+        ></div>
       )}
-      <div className="mt-2 p-2">
-        {/* <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-          CATEGORY
-        </h3> */}
-        <h2 className="text-gray-900 title-font text-lg font-medium">
-          {menuName}
-        </h2>
-        <p className="mt-1">{menuPrice} THB</p>
+      <div class="w-full mb-1 border-grey-light rounded-b flex flex-col justify-between leading-normal relative" >
+        <div class="text-black font-normal text-xl ">{menuName}</div>
+        {/* <p class="text-grey-darker text-base">{description}</p> */}
+        <div className="absolute bottom-0 right-0 pr-3">{menuPrice} ฿</div>
       </div>
     </div>
-    // <div
-    //   class={`max-w-md w-full flex border-b border-grey-light ${
-    //     image === '' ? 'h-16' : ''
-    //   } md:my-2 mx-auto md:rounded-r md:shadow-md md:border-0`}
-    // >
-    //   {image && (
-    //     <div
-    //       class="m-1 h-24 w-1/4 flex-none bg-cover rounded text-center overflow-hidden"
-    //       style={{ backgroundImage: `url('${image}')` }}
-    //     ></div>
-    //   )}
-    //   <div class="w-full mb-5 border-grey-light bg-white rounded-b p-4 flex flex-col justify-between leading-normal relative">
-    //     <div class="mb-6">
-    //       <div class="text-black font-normal text-xl ">{menu}</div>
-    //       {/* <p class="text-grey-darker text-base">{description}</p> */}
-    //       <div className="absolute bottom-0 right-0 pr-6">{price} ฿</div>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
