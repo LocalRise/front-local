d6b1b7be0db4d1e052f8bbb7894bc3c54f657553
dragonfly161518 <por.nattapat.01@gmail.com>
2020-06-13 18:47:28 +0700

    unchange commit

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useMerchant, useCart } from '../../contexts'
 
 const Checkout = ({ location }) => {
-  const id = new URLSearchParams(location.search).get('id')
+  const { id } = useParams()
   const { merchants, fetchMerchantById } = useMerchant()
 
-  return <div class="w-full max-w-screen-xl mx-auto px-6">Checkout</div>
+  useEffect(() => {
+    fetchMerchantById(id)
+  }, [])
+
+  const merchant = merchants[id]
+
+  console.log(merchant)
+
+  return (
+    <div class="w-full max-w-screen-xl mx-auto px-6">
+      <div className="text-xl">1 รายละเอียดการจัดส่ง</div>
+    </div>
+  )
 }
 
 export default Checkout
