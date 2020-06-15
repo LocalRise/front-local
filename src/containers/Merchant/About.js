457af836e530fba25d1660c120ff5ec121234278
PoomSC <poomsc@users.noreply.github.com>
2020-06-15 09:31:27 +0700

    add open close time

--- src/containers/Merchant/About.js
+++ src/containers/Merchant/About.js
@@ -7,8 +7,9 @@ import { FaFacebook } from 'react-icons/fa'
 import { IoIosCall } from 'react-icons/io'
 import { FiPhoneOutgoing } from 'react-icons/fi'
 
-const About = ({ merchant:{description,facebook,foodType,id,image,location,name,openingTime,payment,tel} }) => {
+const About = ({ merchant: { description, facebook, foodType, id, image, location, name, openingTime, openingDay, payment, tel } }) => {
   // dropdown props
+  const dayWork = "Sun Mon Tue Wed Thur Fri Sat"
   const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false)
   const btnDropdownRef = React.createRef()
   const popoverDropdownRef = React.createRef()
@@ -113,14 +114,25 @@ const About = ({ merchant:{description,facebook,foodType,id,image,location,name,
               </a>
 
               <div className="h-0 my-2 border border-solid border-t-0 border-gray-900 opacity-25" />
-
-              {/* {openingTime && (
-                <p className="text-base py-2 px-4 font-normal block w-full bg-transparent text-gray-800 w-full flex flex-warp">
+              {openingTime && (
+                <p className="flex text-base py-2 px-4 font-normal block w-full bg-transparent text-gray-800 w-full">
                   <MdAccessTime
-                    style={{ fontSize: '1.6em', margin: '0 20px 0 0' }}
-                  />{' '}
+                    className="my-auto mr-4"
+                    style={{ fontSize: '1.6em'}}
+                  />
+                  <div>
+                    {dayWork.toString().split(" ").map(val => {
+                      return (
+                        <div>
+                          <div className={`flex text-lg ${Date().toString().substring(0, 3).toLowerCase() === val.toLowerCase() ? "text-blue-700" : "text-gray-500"}`}>
+                            <div className="w-12">{val + "\t"}</div>
+                            {openingDay.split(" ").includes(val) ? `${openingTime}` : "CLOSE"}</div>
+                        </div>
+                      )
+                    })}
+                  </div>
                 </p>
-              )} */}
+              )}
               <p
                 className={
                   'text-sm py-2 px-4 font-normal block w-full bg-transparent text-gray-800 w-full flex flex-warp'
--- src/containers/Merchant/index.js
+++ src/containers/Merchant/index.js
@@ -61,6 +61,7 @@ const Merchant = ({ openCart }) => {
     <div class="w-full max-w-screen-xl mx-auto px-6">
       {merchant &&
         <div className="">
+          {console.log("merchants",merchant)}
           <Cover merchant={merchant} />
           <About merchant={merchant} />
         </div>
